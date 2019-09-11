00e6e05a917cabc2dbe89d2f611aec2173df8da0
hhaslam11 <kalebmarc@hotmail.com>
2019-09-10 19:20:19 -0700

    Added createNewTweet function that will create a dom object of the tweet to later be rendered onto the page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,29 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+const getDaysSince = timestamp => {
+  const currentDate = new Date();
+  const daysSinceTimestamp = currentDate.getTime() - Number(timestamp);
+  return Math.floor(daysSinceTimestamp / (24 * 3600 * 1000));
+};
 
+/* eslint-disable no-undef */
+const createNewTweet = data => {
+  const tweet = $("<article>").addClass("tweet");
+  $(tweet).append(`
+    <header>
+      <span class="icon">
+        <img src="${data.user.avatars}">
+      </span>
+
+      <span class="name">${data.user.name}</span>
+
+      <span class="handle">
+        ${data.user.handle}
+      </span>
+    </header>
+    <p>${data.content.text}</p>
+    <hr>
+    <footer>
+      ${getDaysSince(data.created_at)} days ago
+    </footer>
+  `);
+  return tweet;
+};
