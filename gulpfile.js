58a6655478c8251b884b83c0589a2e079c3bac61
Kaleb Haslam <kalebmarc@hotmail.com>
2019-09-13 04:25:08 +0000

    installed gulp-nodemon but i cant get it to work

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,23 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const nodemon = require('gulp-nodemon');
 
 gulp.task('sass', () => {
   return gulp.src(['./src/sass/*.scss'])
     .pipe(sass())
     .pipe(gulp.dest('./public/styles'));
+});
+
+gulp.task('server', () => {
+
+});
+
+gulp.task('local', () => {
+  nodemon({
+    script: 'server/index.js',
+    ext: 'scss js',
+    tasks: ['sass']
+  });
 });
 
 gulp.task('default', () => {
